a209895f5edf9a2431da27b34236d7ce96f2c727
maximilian27 <maximilian.both27@gmail.com>
2025-01-31 09:53:31 +0200

    feat: add prettier

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,58 +6,58 @@ import { v4 as uuidv4 } from 'uuid';
 import { FormControl, FormGroup, ReactiveFormsModule, UntypedFormGroup } from '@angular/forms';
 
 interface PaginationParams {
-    pageNumber: number;
-    pageSize: number;
+  pageNumber: number;
+  pageSize: number;
 }
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.scss',
-    imports: [ReactiveFormsModule],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+  imports: [ReactiveFormsModule],
 })
 export class AppComponent implements OnInit {
-    private subject: WebSocketSubject<any> | undefined;
+  private subject: WebSocketSubject<any> | undefined;
 
-    public messages: any[] = [];
+  public messages: any[] = [];
 
-    private paginationParamsSubject = new Subject<PaginationParams>();
+  private paginationParamsSubject = new Subject<PaginationParams>();
 
-    protected userId = uuidv4();
+  protected userId = uuidv4();
 
-    protected messageForm = new FormGroup({
-        message: new FormControl('', { nonNullable: true, validators: [] }),
-    });
+  protected messageForm = new FormGroup({
+    message: new FormControl('', { nonNullable: true, validators: [] }),
+  });
 
-    private platformId = inject(PLATFORM_ID);
+  private platformId = inject(PLATFORM_ID);
 
-    public ngOnInit(): void {
-        if (isPlatformBrowser(this.platformId)) {
-            this.subject = new WebSocketSubject({ url: 'ws://localhost:8080/comments' }); // Replace with your WebSocket server URL
-            this.subject.pipe(catchError((error) => of(error))).subscribe((response: any) => {
-                console.log('Message received:', response);
-                this.messages = response.allMessages;
-            });
-        }
-
-        // Send pagination params on initial connection
-        // this.paginationParamsSubject.next({ pageNumber: 1, pageSize: 10 });
-        //
-        // // Send pagination params on changes
-        // this.paginationParamsSubject.subscribe((params: PaginationParams) => {
-        //   this.subject!.next({
-        //     type: 'pagination',
-        //     payload: params
-        //   });
-        // });
+  public ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      this.subject = new WebSocketSubject({ url: 'ws://localhost:8080/comments' }); // Replace with your WebSocket server URL
+      this.subject.pipe(catchError((error) => of(error))).subscribe((response: any) => {
+        console.log('Message received:', response);
+        this.messages = response.allMessages;
+      });
     }
 
-    protected sendMessage(formValue: any) {
-        this.subject!.next({ userId: this.userId, text: formValue.message });
-    }
+    // Send pagination params on initial connection
+    // this.paginationParamsSubject.next({ pageNumber: 1, pageSize: 10 });
+    //
+    // // Send pagination params on changes
+    // this.paginationParamsSubject.subscribe((params: PaginationParams) => {
+    //   this.subject!.next({
+    //     type: 'pagination',
+    //     payload: params
+    //   });
+    // });
+  }
 
-    // Example of how to change pagination params
-    changePagination(pageNumber: number, pageSize: number) {
-        this.paginationParamsSubject.next({ pageNumber, pageSize });
-    }
+  protected sendMessage(formValue: any) {
+    this.subject!.next({ userId: this.userId, text: formValue.message });
+  }
+
+  // Example of how to change pagination params
+  changePagination(pageNumber: number, pageSize: number) {
+    this.paginationParamsSubject.next({ pageNumber, pageSize });
+  }
 }
